2680ea92dd8f6f563fb3992304777d81270a9d3d
kthoons <kth9987992@naver.com>
2024-07-03 02:27:43 +0900

    202407030227

---
+++ popup.js
@@ -0,0 +1,8 @@
+document.getElementById('get-url').addEventListener('click', function() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      var activeTab = tabs[0];
+      var activeTabUrl = activeTab.url;
+      document.getElementById('url').textContent = activeTabUrl;
+    });
+  });
+  
